dc8ccfda5c3e05d554f840412252b2e36c8ad927
JGastager <jgastager@gmx.at>
2023-10-31 20:32:37 +0100

    migrating to intersection observer

--- src/in-viewport-directive.js
+++ src/in-viewport-directive.js
@@ -1,33 +1,12 @@
 export default function InViewportDirective(globalOptions) {
     return {
-        mounted(el, binding, vnode) {
+        mounted(el, binding) {
             // Merge local options (provided in the directive's binding value) with global options
             const options = { ...globalOptions, ...binding.value };
 
             // Add generell in-viewport class
             el.classList.add(options.classDefault);
 
-            // Throttle function utility
-            function throttle(func, delay) {
-                let lastCall = 0;
-                return function (...args) {
-                    const now = new Date().getTime();
-                    if (now - lastCall >= delay) {
-                        lastCall = now;
-                        func(...args);
-                    }
-                };
-            }
-
-            // Debounce function utility
-            function debounce(func, delay) {
-                let timeoutId;
-                return function (...args) {
-                    clearTimeout(timeoutId);
-                    timeoutId = setTimeout(() => func(...args), delay);
-                };
-            }
-            
             // Add class function
             function addClass(el, classDefault, delay) {
                 setTimeout(() => {
@@ -46,47 +25,38 @@ export default function InViewportDirective(globalOptions) {
                 }, delay);
             }
 
-            function checkViewport() {
-                const rect = el.getBoundingClientRect();
-                const isInViewport =
-                    rect.top + options.offsetBottom <= window.innerHeight && rect.bottom - options.offsetTop >= 0;
-                if (isInViewport) {
-                    addClass(el, options.classViewed, options.delay);
-                    if (!options.triggerOnce) {
-                        addClass(el, options.classActive, options.delay);
-                    } else {
-                        // if there shouldn't be an active state, remove event listener after triggering once
-                        window.removeEventListener("scroll", checkViewport);
+            //Checks if an element is in the viewport and adds or removes classes accordingly
+            function checkViewport(entries) {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        addClass(el, options.classViewed, options.delay);
+                        if (!options.triggerOnce) {
+                            addClass(el, options.classActive, options.delay);
+                        } else {
+                            // if there shouldn't be an active state, remove intersection observer after triggering once
+                            observer.unobserve(el);
+                        }
+                    } else if (!options.triggerOnce) {
+                        removeClass(el, options.classActive, options.delay);
                     }
-                } else if (!options.triggerOnce) {
-                    removeClass(el, options.classActive, options.delay);
-                }
+                });
             }
 
-            // Throttle the checkViewport function with a reasonable delay
-            const throttledCheckViewport = throttle(checkViewport, 100);
-
-            // Debounce the checkViewport function for resize events
-            const debouncedCheckViewport = debounce(checkViewport, 100);
-
-            // Add the throttled function to the scroll event listener
-            window.addEventListener("scroll", throttledCheckViewport);
-
-            // Add the debounced function to the resize event listener
-            window.addEventListener("resize", debouncedCheckViewport);
+            // Create a new intersection observer instance with the given options
+            const observer = new IntersectionObserver(checkViewport, {
+                rootMargin: `${options.offsetTop}px 0px ${options.offsetBottom}px 0px`,
+                threshold: options.threshold / 100,
+            });
 
             // Initial check
-            checkViewport();
+            observer.observe(el);
 
             // Attach the function to the element for later cleanup
-            el._throttledCheckViewport = throttledCheckViewport;
-            el._debouncedCheckViewport = debouncedCheckViewport;
+            el._observer = observer;
         },
-
         beforeUnmount(el) {
             // Cleanup
-            window.removeEventListener("scroll", el._throttledCheckViewport);
-            window.removeEventListener("resize", el._debouncedCheckViewport);
+            el._observer.unobserve(el);
         },
     };
 }
